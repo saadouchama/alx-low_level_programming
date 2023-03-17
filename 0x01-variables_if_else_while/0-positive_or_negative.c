e02482d6bc87865c0b223ff5f1a2ec008464e4fe
Saad Ouchama <saadochama@gmail.com>
2023-03-17 12:39:56 -0700

    task: 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,12 @@
 #include <time.h>
 /* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+ * main - prints exactly "and that piece of art is useful"
+ * @author - "Saad Ouchama", "2015-10-19",
+ * @brief "this main fonction check if the number positive or negative or equal to 0 and print the result"
+ * @return "Always 0 (Success)"
+ */
 int main(void)
 {
 	int n;
